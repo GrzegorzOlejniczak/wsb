f51996c6ede730c52867e27e2ddcf3933c47b199
GrzegorzOlejniczak <55291443+GrzegorzOlejniczak@users.noreply.github.com>
2022-10-22 14:59:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace kolkokrzyzyk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //zad1
+            //int a;
+            //Console.Write("Podaj liczbe: ");
+            //a = int.Parse(Console.ReadLine());
+
+
+            //if (a % 2 == 0)
+            //{
+            //    Console.WriteLine("liczba jest parzysta");
+            //}
+            //else
+            //{
+            //    Console.Write("Liczba nie jest parzysta");
+            //}
+
+
+            //zad2
+
+            //int a, b;
+            //char znak = ' ';
+
+            //Console.Write("Podaj a: ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("Operacja: ");
+            //znak = char.Parse(Console.ReadLine());
+            //Console.Write("Podaj b: ");
+            //b = int.Parse(Console.ReadLine());
+
+            //switch (znak)
+            //{
+            //    case '+':
+            //        Console.WriteLine(a + b);
+            //        break;
+            //    case '-':
+            //        Console.WriteLine(a - b);
+            //        break;
+            //    case '*':
+            //        Console.WriteLine(a * b);
+            //        break;
+            //    case '/':
+            //        Console.WriteLine(a / b);
+            //        break;
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
