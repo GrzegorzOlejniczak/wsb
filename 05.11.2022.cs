4d3ff31b841eed6047662b0ad0efe42d94f1c624
GrzegorzOlejniczak <55291443+GrzegorzOlejniczak@users.noreply.github.com>
2022-11-05 15:00:19 +0100

    Update 05.11.2022.cs

--- 05.11.2022.cs
+++ 05.11.2022.cs
@@ -57,6 +57,10 @@ namespace ConsoleApp13
             }
             */
 
+
+
+            //Liczba pierwsza
+            /*
             int n;
             bool czyPierwsza = true;
             Console.Write("Podaj liczbę: ");
@@ -77,18 +81,91 @@ namespace ConsoleApp13
             {
                 Console.Write("Liczba jest pierwsza");
             }
+
+            */
+            //suma szescianow liczb miedzu n a m 
+
+
+            /*
+            int n=0, m;
+            int suma=0;
+            Console.Write("Podaj n: ");
+            while (true)
+            {
+                try
+                {
+                    n = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException FormatException)
+                {
+                    Console.WriteLine(FormatException.Message);
+                }
+                catch(OverflowException OverflowException)
+                {
+                    Console.WriteLine(OverflowException.Message);
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Jakis inny wyjatek");
+                }
+            }
+
+            Console.Write("Podaj m: ");
+            m = int.Parse(Console.ReadLine());
             
+            for(int i =n; i<=m; i++)
+            {
+                suma += (int)Math.Pow(i, 3);
+            }
+
+            Console.Write(suma);
+
+            */
+
+            int n;
+            Console.Write("Podaj długość ciągu:");
+            n = int.Parse(Console.ReadLine());
 
 
 
+            double[] tab = new double[n];
 
 
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine("Podaj liczbe o indeksie " + i + " :");
+                tab[i] = double.Parse(Console.ReadLine());
+                
+            }
+
+            Console.WriteLine();
+            Console.Write("Kolumna: ");
+            for(int i =0; i<tab.Length; i++)
+            {
+                Console.WriteLine();
+                Console.Write(tab[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Wiersz: ");
+            for (int i = 0; i < tab.Length; i++)
+            {
+                
+                Console.Write(tab[i]+",");
+            }
 
 
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("odwrotna kolejność: ");
 
 
-
-
+            for(int i = tab.Length-1; i>=0; i--)
+            {
+                Console.Write(tab[i]+",");
+            }
         }
     }
 }
