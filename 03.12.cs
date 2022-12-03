95c29a983012be9fdcf6a3dad23a8422ffe2a2aa
GrzegorzOlejniczak <55291443+GrzegorzOlejniczak@users.noreply.github.com>
2022-12-03 12:12:55 +0100

    Update 03.12.cs

--- 03.12.cs
+++ 03.12.cs
@@ -1,1 +1,37 @@
 
+uint a, b;
+            Console.Write("Podaj a: ");
+           
+            
+
+            while (true)
+            {
+                try
+                {
+                    a = uint.Parse(Console.ReadLine());
+                    Console.Write("Podaj b: ");
+                    b = uint.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException FormatException)
+                {
+                    Console.WriteLine(FormatException.Message);
+                    ;
+                    
+                }
+                
+            }
+
+            for (int i =1; i<10; i++)
+            {
+                for(int j=1; j<10; j++)
+                {
+
+                Console.Write(i*a+j*b+" ");
+
+
+
+                }
+                    Console.WriteLine();
+
+            }
