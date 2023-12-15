f156bb3644e889bd4782566ff34d10ba7921673f
GrzegorzOlejniczak <55291443+GrzegorzOlejniczak@users.noreply.github.com>
2023-12-15 21:25:18 +0100

    Add files via upload

---
+++ WinFormsApp632/SortItemsListView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+public class ListViewColumnSorter : IComparer
+{
+    private int ColumnToSort;
+    private SortOrder OrderOfSort;
+    private CaseInsensitiveComparer ObjectCompare;
+
+    public ListViewColumnSorter()
+    {
+        ColumnToSort = 0;
+        OrderOfSort = SortOrder.None;
+        ObjectCompare = new CaseInsensitiveComparer();
+    }
+
+    public int Compare(object x, object y)
+    {
+        ListViewItem listviewX = (ListViewItem)x;
+        ListViewItem listviewY = (ListViewItem)y;
+
+        if (IsSpecialItem(listviewX) || IsSpecialItem(listviewY))
+        {
+            return 0;
+        }
+
+        if (ColumnToSort == 1)
+        {
+            DateTime dateX = DateTime.Parse(listviewX.SubItems[ColumnToSort].Text);
+            DateTime dateY = DateTime.Parse(listviewY.SubItems[ColumnToSort].Text);
+
+            int compareResult = DateTime.Compare(dateX, dateY);
+
+            return (OrderOfSort == SortOrder.Ascending) ? compareResult : -compareResult;
+        }
+        else
+        {
+            int compareResult = ObjectCompare.Compare(listviewX.SubItems[ColumnToSort].Text, listviewY.SubItems[ColumnToSort].Text);
+
+            return (OrderOfSort == SortOrder.Ascending) ? compareResult : -compareResult;
+        }
+    }
+    public int SortColumn
+    {
+        set { ColumnToSort = value; }
+        get { return ColumnToSort; }
+    }
+
+    public SortOrder Order
+    {
+        set { OrderOfSort = value; }
+        get { return OrderOfSort; }
+    }
+
+    private bool IsSpecialItem(ListViewItem item)
+    {
+        return item.Text.StartsWith("<-.........");
+    }
+}
